7edd47709b24a0c0ce8ac05eba4d7c10e28e4c0e
Joachim Krøyer <jok@lindcapital.com>
2021-11-22 22:26:19 +0100

    Initial commit

---
+++ swafe_react_assignment/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div className=" bg-gray-600 " >
+            <header>
+                <p>
+                    login here
+                </p>
+            </header>
+        </div>
+    );
+}
+export default Login;
